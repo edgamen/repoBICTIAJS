0851869aad61d10d599c30bc31f34e54112b8e2f
Edgardo <edgamencontacto@gmail.com>
2018-10-31 23:45:42 -0500

    Issues v2
    Se completa issue 1, issue 2 sigue abierto y  el issue 5 se resuelve de forma parcial.

--- sesion12/Taller 3/scripts/javascript.js
+++ sesion12/Taller 3/scripts/javascript.js
@@ -6,34 +6,34 @@ document.getElementById("mute").onclick = function(){
     if(volumen == 1){
         document.getElementsByTagName("video")[0].volume = 0;
         volumen = 0;
+
     }else if(volumen == 0){
         document.getElementsByTagName("video")[0].volume = 1.0;
         volumen = 1;
+        
     }
 }
-/*Fin de la funcion que controla el audio del video*/
+/*
 
-$(document).ready(function(){
-    $(window).scroll(function(){
-        var scroll = $(window).scrollTop();
-        if (scroll > 300) {
-          $(".black").css("background" , "blue");
+Fin de la funcion que controla el audio del video*/
+/* Se ubica un metodo en el espacio del navegador para escuchar los eventos y al presentarse el scroll, basandose en el
+ id nav y comparando para dos tipos de navegadores y asi agregar y remover las clases pertinentes*/
+window.addEventListener('scroll', function (e) {
+    var nav = document.getElementById('nav');
+    if (document.documentElement.scrollTop || document.body.scrollTop > window.innerHeight) {
+            nav.classList.add('nav-colored');
+            nav.classList.remove('nav-transparent');
+        } else {
+            nav.classList.add('nav-transparent');
+            nav.classList.remove('nav-colored');
         }
-  
-        else{
-            $(".black").css("background" , "#333");  	
-        }
-    })
-  })
+});
 
-  function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 4,
-      center: {lat: -33, lng: 151},
-      mapTypeControl: false,
-      mapTypeControlOptions: {
-        style: google.maps.MapTypeControlStyle.DROPDOWN_MENU,
-        mapTypeIds: ['roadmap', 'terrain']
-      }
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 10,
+      center: {lat: 4.620824, lng: -74.063659},
+      disableDefaultUI: true
     });
   }
+  initMap();
